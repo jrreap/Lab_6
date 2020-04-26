9d86380dd00d133b6ee2ce7da6ea3d3545aab720
ToastedMacaroon <olympus@udel.edu>
2020-04-26 15:31:37 -0400

    testing #2

--- java181/src/main/java/cisc181/Lab4/java181/Action.java
+++ java181/src/main/java/cisc181/Lab4/java181/Action.java
@@ -2,8 +2,28 @@ package cisc181.Lab4.java181;
 
 public abstract class Action {
     Game181 game;
-    int currentRow;
-    int currentColumn;
-    int desiredRow;
-    int desiredColumn;
+    int fromSpaceRow;
+    int fromSpaceColumn;
+    int toSpaceRow;
+    int toSpaceColumn;
+
+    public int getFromSpaceColumn() {
+        return fromSpaceColumn;
+    }
+
+    public int getFromSpaceRow() {
+        return fromSpaceRow;
+    }
+
+    public int getToSpaceColumn() {
+        return toSpaceColumn;
+    }
+
+    public int getToSpaceRow() {
+        return toSpaceRow;
+    }
+
+    public Game181 getGame() {
+        return game;
+    }
 }
