a9d0cae6054a90503ff3fbf3c6b33c7389448e6f
ToastedMacaroon <olympus@udel.edu>
2020-04-26 23:25:05 -0400

    this is the nextplayersaction function to the best of my tired brain at 11 ocloock pm. I seperated the from row and from colomns into two seperate scanners, same with the to row and tocol.

--- java181/src/test/java/cisc181/Lab4/java181/PlayGame.java
+++ java181/src/test/java/cisc181/Lab4/java181/PlayGame.java
@@ -37,5 +37,41 @@ public class PlayGame {
 
     private void nextPlayersAction(){
         char action = getValidActionType();
+        boolean notValidturn = true;
+        Scanner scanner = new Scanner(System.in);
+        while(notValidturn) {
+            System.out.println("Enter in the from row");
+            int fromRow = scanner.nextInt();
+            System.out.println("Enter in the from column");
+            int fromColumn = scanner.nextInt();
+            System.out.println("Enter in the to row");
+            int toRow = scanner.nextInt();
+            System.out.println("Enter in the to column");
+            int toCol = scanner.nextInt();
+            if (action == 'm') {
+                ActionMove moving = new ActionMove(game, fromRow, fromColumn, toRow, toCol);
+                if (moving.validAction()) {
+                    moving.performAction();
+                    notValidturn = false;
+                }
+            }
+            else if (action == 'r') {
+                ActionRecruit recruiting = new ActionRecruit(game, fromRow, fromColumn, toRow, toCol);
+                if (recruiting.validAction()) {
+                    recruiting.performAction();
+                    notValidturn = false;
+                }
+            }
+            else if (action == 'a') {
+                ActionAttack attacking = new ActionAttack(game, fromRow, fromColumn, toRow, toCol);
+                if (attacking.validAction()) {
+                    attacking.performAction();
+                    notValidturn = false;
+                }
+            }
+        }
+    }
+    public void playOurGame(){
+        nextPlayersAction();
     }
 }
