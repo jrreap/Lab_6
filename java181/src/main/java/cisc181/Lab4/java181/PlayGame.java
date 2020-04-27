c0a9d5094a9b08b8e2fa45ea742880b8b4bcdcbb
ToastedMacaroon <olympus@udel.edu>
2020-04-27 16:39:25 -0400

    fixed somethings

--- java181/src/main/java/cisc181/Lab4/java181/PlayGame.java
+++ java181/src/main/java/cisc181/Lab4/java181/PlayGame.java
@@ -13,25 +13,10 @@ public class PlayGame {
         String character = "";
         Scanner scanner = new Scanner(System.in);
         while ((character != "m") && (character != "r") && (character != "a")) {
-            System.out.println("Please enter either a m, r , or a key value");
+            System.out.println("M:move A:attack R:recruit");
             character = scanner.next();
         }
-        if (character == "m") {
-            System.out.println("player moves piece from from space selected to the to space selected");
-            System.out.println("from space must contain a piece belonging to team making the move");
-            System.out.println("to space must by empty");
-            returnValue = 'm';
-        } else if (character == "r") {
-            System.out.println("player selects piece from from space selected to recruit the piece in the to space selected");
-            System.out.println("from space must contain a piece belonging to team whose turn it is -this piece must have the ability to recruit");
-            System.out.println("to space must contain a piece belonging to the other team");
-            returnValue = 'r';
-        } else if (character == "a") {
-            System.out.println("player selects piece from from space selected to attack the piece in the to space selected");
-            System.out.println("from space must contain a piece belonging to team whose turn it is -this piece must have the ability to attack");
-            System.out.println("to space must contain a piece belonging to the other team");
-            returnValue = 'a';
-        }
+        returnValue = character.charAt(0);
         return returnValue;
     }
 
@@ -72,6 +57,7 @@ public class PlayGame {
 
     public void playOurGame() {
         nextPlayersAction();
-
+        BoardSpace[][] space = game.getBoard().getSpaces();
+        Piece piece =
     }
 }
